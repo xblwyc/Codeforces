04e0996a4b3dd5368f937686b2b1185f8a03faad
xblwyc <171732223@qq.com>
2016-06-11 13:49:25 +0800

    CF round 356

---
+++ CF Round 356 Div1/A.cpp
@@ -0,0 +1,74 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <ostream>
+#include <queue>
+#include <cmath>
+#include <climits>
+#include <utility>
+#include <fstream>
+#include <memory>
+#include <sstream>
+#include <set>
+#include <iterator>
+#include <iomanip>
+#include <map>
+#include <stack>
+using namespace std;
+#define FOR(a, b, n) for(int (a) = (b); (a) < (n); ++(a))
+#define FORE(a, b, n) for(int (a) = (b); (a) <= (n); ++(a))
+#define ITE(a, v) for(auto (a) = v.begin(); (a) != v.end(); ++(a))
+#define LL long long
+#define ALL(v) v.begin(),v.end()
+#define ZERO(v) memset(v, 0, sizeof v)
+#define NEG(v)  memset(v, -1, sizeof v)
+#define F first
+#define S second
+#define LD double
+#define pw(x) (1LL << (x))
+#define sqr(x) ((x)*(x))
+#define PB push_back
+#define MP make_pair
+#define MOD 1000000007
+#define PI 3.141592653589793
+#define PII pair<LL, LL>
+#define INF 0x3f3f3f3f
+#define FUL(x) memset(x, 0x3f, sizeof(x));
+#define debug(args...) {cerr << #args << " = "; errDebug(args); cerr << endl;}
+void errDebug() {}
+template<typename T, typename... Args>
+void errDebug(T a, Args... args) {
+    cerr << a << ' ';
+    errDebug(args...);
+}
+int prime[25] = {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47};
+int main()
+{
+    cin.tie(0);
+    cin.sync_with_stdio(false);
+    int c = 0;
+    string s;
+    FOR(i,0,15)
+    {
+        cout << prime[i] << endl;
+        cin >> s;
+        if(s == "yes")
+            c++;
+    }
+    FOR(i,0,4)
+    {
+        cout << prime[i] * prime[i] << endl;
+        cin >> s;
+        if(s == "yes")
+            c = INF;
+    }
+    if(c >= 2)
+        cout << "composite" << endl;
+    else
+        cout << "prime" << endl;
+    return 0;
+}
